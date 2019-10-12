c83680ab266228be3a823f172fbde46c39b16304
Ananda Padmanabhan <Ananda Padmanabhan@DESKTOP-J903RND>
2019-10-12 22:39:04 +0530

    TO do clock en and dis

--- GPIO_Scratch/drivers/inc/stm32g071xx.h
+++ GPIO_Scratch/drivers/inc/stm32g071xx.h
@@ -71,6 +71,13 @@ typedef struct
 	 	_IO uint32_t BRR;						/*BIT RESET REGISTER					OFFSET 0x28*/
 		}GPIO_REG_DEF;
 /*******************************************************************************************************************************************/
+
+#define GPIOA								 (GPIO_REG_DEF*)PORT_GPIOA
+#define GPIOB								 (GPIO_REG_DEF*)PORT_GPIOB
+#define GPIOC								 (GPIO_REG_DEF*)PORT_GPIOC
+#define GPIOD								 (GPIO_REG_DEF*)PORT_GPIOD
+#define GPIOF								 (GPIO_REG_DEF*)PORT_GPIOF
+
 /*************************************************SPI***************************************************************************************/
 	typedef struct
 		{
@@ -85,7 +92,11 @@ typedef struct
 		_IO uint32_t I2SPR;						/*I2S PRESCALAR REGISTER				OFFSET 0x20*/
 		}SPI_REF_DEF;
 /******************************************************************************************************************************************/
-/************************************************I2C*************************************************************************************/
+
+#define	SPI1								(SPI_REF_DEF*)SPI1_BASE_ADDR
+#define	SPI2								(SPI_REF_DEF*)SPI2_BASE_ADDR
+
+/************************************************I2C***************************************************************************************/
 	typedef struct
 		{
 		_IO uint32_t CR1;						/*CONTROL REGISTER 1 					OFFSET 0x00*/
@@ -101,6 +112,10 @@ typedef struct
 		_IO uint32_t TXDR;						/*TX DATA REGISTER						OFFSET 0x28*/
 		}I2C_REG_DEF;
 /******************************************************************************************************************************************/
+
+#define I2C1								(I2C_REG_DEF*)I2C1_BASE_ADDR
+#define I2C2								(I2C_REG_DEF*)I2C2_BASE_ADDR
+
 /***********************************************USART**************************************************************************************/
 	typedef struct
 		{
@@ -118,6 +133,10 @@ typedef struct
 		_IO uint32_t PRESC;						/*PRESCALAR								OFFSET 0X2C*/
 		}USART_REG_DEF;
 /*******************************************************************************************************************************************/
+#define USART1								(USART_REG_DEF*)USART1_BASE_ADDR
+#define USART2								(USART_REG_DEF*)USART2_BASE_ADDR
+#define USART3								(USART_REG_DEF*)USART3_BASE_ADDR
+#define USART4								(USART_REG_DEF*)USART4_BASE_ADDR
 /*************************************************EXTI**************************************************************************************/
 	typedef struct
 		{
@@ -137,8 +156,9 @@ typedef struct
 		_IO uint32_t RESERVE3[2];				/*RESERVED3 							OFFSET	0x88-0x8C*/
 		_IO uint32_t IMR2;						/*WAKEUPWITH INTERRUPTMASK REGISTER2	OFFSET 	0x90*/
 		_IO uint32_t EMR2;						/**WAKEUPWITH EVENTMASK REGISTER2		OFFSET 	0x94*/
-		};
+		}EXTI_REG_DEF;
 /******************************************************************************************************************************************/
+#define EXTI								(EXTI_REG_DEF*)EXTI_BASE_ADDR;
 /*************************************************RCC**************************************************************************************/
 	typedef struct
 		{
@@ -167,10 +187,50 @@ typedef struct
 		_IO uint32_t RESERVED3;					/*RESERVED3								OFFSET	0x58*/
 		_IO uint32_t BDCR;						/*RTC DOMAIN CONTROL REGISTER			OFFSET	0x5C*/
 		_IO uint32_t CSR;						/*CONTROL STATUS REGISTER				OFFSET	0X60*/
-		};
+		}RCC_REG_DEF;
 /******************************************************************************************************************************************/
+#define RCC									(RCC_REG_DEF*)RCC_BASE_ADDR;
 /*************************************************SYSCGR***********************************************************************************/
-
+	typedef struct
+		{
+		_IO uint32_t CFGR1;						/*CONFIGURATION REGISTER1				OFFSET	0x00*/
+		_IO uint32_t RESERVED[5];				/*RESERVED								OFFSET	0x04-0x17*/
+		_IO uint32_t CFGR2;						/*CONFIGURATION REGISTER2				OFFSET	0x18*/
+		_IO uint32_t RESERVED2[25];				/*RESERVED								OFFSET	0x20-0x7C*/
+		_IO uint32_t ITLINE0;					/*INTERRUPT LINE STATUS REGISTER0		OFFSET	0x80*/
+		_IO uint32_t ITLINE1;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0x84*/
+		_IO uint32_t ITLINE2;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0x88*/
+		_IO uint32_t ITLINE3;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0x8C*/
+		_IO uint32_t ITLINE4;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0x90*/
+		_IO uint32_t ITLINE5;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0x94*/
+		_IO uint32_t ITLINE6;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0x98*/
+		_IO uint32_t ITLINE7;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0x9C*/
+		_IO uint32_t ITLINE8;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xA0*/
+		_IO uint32_t ITLINE9;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xA4*/
+		_IO uint32_t ITLINE10;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xA8*/
+		_IO uint32_t ITLINE11;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xAC*/
+		_IO uint32_t ITLINE12;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xB0*/
+		_IO uint32_t ITLINE13;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xB4*/
+		_IO uint32_t ITLINE14;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xB8*/
+		_IO uint32_t ITLINE15;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xBC*/
+		_IO uint32_t ITLINE16;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xC0*/
+		_IO uint32_t ITLINE17;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xC4*/
+		_IO uint32_t ITLINE18;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xC8*/
+		_IO uint32_t ITLINE19;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xCC*/
+		_IO uint32_t ITLINE20;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xD0*/
+		_IO uint32_t ITLINE21;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xD4*/
+		_IO uint32_t ITLINE22; 					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xD8*/
+		_IO uint32_t ITLINE23;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xDC*/
+		_IO uint32_t ITLINE24;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xE0*/
+		_IO uint32_t ITLINE25;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xE4*/
+		_IO uint32_t ITLINE26;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xE8*/
+		_IO uint32_t ITLINE27;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xEC*/
+		_IO uint32_t ITLINE28;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xF0*/
+		_IO uint32_t ITLINE29;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xF4*/
+		_IO uint32_t ITLINE30;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xF8*/
+		_IO uint32_t ITLINE31;					/*INTERRUPT LINE STATUS REGISTER1		OFFSET	0xFC*/
+		}SYS_CFGR_REG_DEF;
 /******************************************************************************************************************************************/
+#define SYS_CFGR							(SYS_CFGR_REG_DEF*)SYS_CFG_BASE_ADDR;
 /******************************************************************************************************************************************/
 #endif /* INC_STM32G071XX_H_ */
